80f7e4fb8044ccb4b46c3bbde2dc3fc420d27f2d
Amy Dang <kirontoo@gmail.com>
2021-05-26 14:56:58 -0700

    add toggle layer

--- boardwalk/keymap.c
+++ boardwalk/keymap.c
@@ -28,13 +28,14 @@
 #define ESC_TAP LGUI_T(KC_ESC)
 
 #define GAME TT(_GAME)
+#define BASE TT(_BASE)
 
 // Layer shorthand
 enum layer {
     _BASE,
     _LOWER,
-		_RAISE,
-		_GAME
+    _RAISE,
+    _GAME
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -55,18 +56,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_RAISE] = LAYOUT_ortho_hhkb(
-        RESET,   RGB_TOG, RGB_MOD, _______, _______, _______, GAME,    _______, _______, _______, _______, _______,  _______, KC_MPLY,
+        RESET,   RGB_TOG, RGB_MOD, _______, _______, _______, GAME,    BASE,    _______, _______, _______, _______,  _______, KC_MPLY,
         _______, RGB_HUI, RGB_HUD, _______, _______, _______, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN, _______,
         _______, RGB_SAI, RGB_SAD, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_PIPE, KC_DQT,
         KC_LBRC, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT,  KC_BSLS, KC_RBRC,
                  _______, _______, _______, _______,          _______, _______,          _______, _______, _______, _______
     ),
 
-		[_GAME] = LAYOUT_ortho_hhkb(
+    [_GAME] = LAYOUT_ortho_hhkb(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                 KC_LCTL, KC_LGUI, KC_LALT, _______,          KC_SPC,  KC_BSPC,           _______, _______, _______, _______
+                 KC_LCTL, KC_LGUI, KC_LALT, KC_LWR,           KC_SPC,  KC_BSPC,          KC_RAS, _______, _______, _______
     ),
 };

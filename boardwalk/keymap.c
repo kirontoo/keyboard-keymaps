5b49db39846c69a301feee63543aa71d7df5c2a6
Amy Dang <kirontoo@gmail.com>
2021-06-18 15:45:35 -0700

    change keymap

--- boardwalk/keymap.c
+++ boardwalk/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ortho_hhkb(
         KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PSCR, KC_MPLY, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
         KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_INS,  KC_DEL,  KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
-        KC_CTLEC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_HOME, KC_PGUP, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
+        KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_HOME, KC_PGUP, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
         KC_LST,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_PGDN, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RST,
                   KC_LCTL, KC_LGUI, KC_LALT, KC_LWR,           KC_SPC,  KC_BSPC,          KC_RAS,  KC_RGUI, KC_RALT, KC_RCTL
     ),
@@ -64,9 +64,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LOWER] = LAYOUT_ortho_hhkb(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   EXT_APP, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_UNDS,
-        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_PLUS, KC_F1,   KC_F2,   KC_F3,   EXT_APP, KC_UNDS,
-        KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_EQL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_QUOT,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   EXT_APP, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_UNDS,
+        KC_CAPS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_PLUS, KC_F1,   KC_F2,   KC_F3,   EXT_APP, KC_UNDS,
+        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_EQL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_QUOT,
         KC_LCBR, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, KC_MINS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_RCBR,
                  _______, _______, _______, _______,          _______, _______,          _______, _______, _______, _______
     ),

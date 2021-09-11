93947276528c53ab300c898fc97533196957d926
Amy Dang <kirontoo@gmail.com>
2021-09-11 14:52:57 -0700

    Update planck keymap with encoder

--- planck/config.h
+++ planck/config.h
@@ -41,4 +41,4 @@
 
 #define TAPPING_TERM 200
 #define RETRO_TAPPING
-#define PERMISSIVE_HOLD 
+#define PERMISSIVE_HOLD
--- planck/keymap.c
+++ planck/keymap.c
@@ -27,11 +27,6 @@ enum planck_layers {
 
 enum planck_keycodes {
 	COLEMAK = SAFE_RANGE,
-	// Encoder Mods
-	ENC_ALT,
-	ENC_GUI,
-	ENC_CTRL,
-	G_SWAP,
 	EEQL,
 	NEQL,
 	LEQL,
@@ -75,30 +70,26 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_COLEMAK] = LAYOUT_planck_grid(
-			KC_MPLY,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_TAB,
-			ESC_TAP,   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
-			OSM_LST,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSM_RST,
-			ENC_CTRL,  KC_LGUI,  ENC_GUI, ENC_ALT, OSL_LWR, KC_SPC,  KC_BSPC, OSL_RAS, KC_RGUI, KC_RALT, KC_RALT, KC_RCTRL
+        KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_GRV,
+        ESC_TAP,   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
+        OSM_LST,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSM_RST,
+        KC_LCTRL,  KC_LGUI, KC_LGUI, KC_LALT, OSL_LWR, KC_SPC,  KC_BSPC, OSL_RAS, KC_RGUI, KC_RALT, KC_RALT, KC_RCTRL
 	),
 
 	[_RAISE] = LAYOUT_planck_grid(
-			_______, C_OR,     C_AND,   ME,      PE,      PP,       KC_CIRC, KC_AMPR,  KC_ASTR,   KC_LPRN,  KC_RPRN, KC_GRV,
-			_______, LEQL,     EEQL,    NEQL,    GEQL,    KC_TILDE, KC_LEFT, KC_DOWN,  KC_UP,     KC_RIGHT, KC_PIPE, KC_DQT,
-			KC_LBRC, _______,  _______, _______, _______, MM,       KC_HOME, KC_PGDN,  KC_PGUP,   KC_END,   KC_BSLS, KC_RBRC,
-			_______, _______,  _______, _______, _______, _______,  _______, _______,  KC_MPRV,  KC_VOLD,  KC_VOLU, KC_MNXT
+        _______, C_OR,     C_AND,   ME,      PE,      PP,       KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN,  KC_RPRN, KC_MPLY,
+        _______, LEQL,     EEQL,    NEQL,    GEQL,    KC_TILDE, KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT, KC_PIPE, KC_DQT,
+        KC_LBRC, _______,  _______, _______, _______, MM,       KC_HOME, KC_PGDN,  KC_PGUP, KC_END,   KC_BSLS, KC_RBRC,
+        _______, _______,  _______, _______, _______, _______,  _______, _______,  KC_MPRV, KC_VOLD,  KC_VOLU, KC_MNXT
 	),
 
 	[_LOWER] = LAYOUT_planck_grid(
-			_______,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_PLUS, KC_F1,   KC_F2,   KC_F3,    EXT_APP, KC_UNDS,
-			_______,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_EQL,  KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_QUOT,
-			KC_LCBR,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_RCBR,
-			_______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______
+        _______,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_PLUS, KC_F1,   KC_F2,   KC_F3,    EXT_APP, KC_UNDS,
+        _______,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_EQL,  KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_QUOT,
+        KC_LCBR,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_RCBR,
+        _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______
 	),
-
 };
-
-// flags for encoder and alt gui swap
-bool lalt = false, lctrl = false, lgui = false, ag_swap = true;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	switch (keycode) {
@@ -106,46 +97,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			if (record->event.pressed) {
 				set_single_persistent_default_layer(_COLEMAK);
 			}
-			return false;
-			break;
-		case G_SWAP:
-			if (record->event.pressed) {
-				ag_swap = !ag_swap;
-			}
-			return false;
-			break;
-
-		case ENC_ALT:
-			if (record->event.pressed) {
-				(ag_swap) ? register_code(KC_LALT) : register_code(KC_LGUI);
-				lalt = !lalt;
-			} else {
-				(ag_swap) ? unregister_code(KC_LALT) : unregister_code(KC_LGUI);
-				lalt = !lalt;
-			}
-			return false;
-			break;
-
-		case ENC_GUI:
-			if (record->event.pressed) {
-				(ag_swap) ? register_code(KC_LGUI) : register_code(KC_LALT);
-				lgui = !lgui;
-			} else {
-				(ag_swap) ? unregister_code(KC_LGUI) : unregister_code(KC_LALT);
-				lgui = !lgui;
-			}
-			return false;
-			break;
-
-		case ENC_CTRL:
-			if (record->event.pressed) {
-				register_code(KC_LCTRL);
-				lctrl = !lctrl;
-			} else {
-				unregister_code(KC_LCTRL);
-				lctrl = !lctrl;
-			}
-			return false;
+		return false;
 			break;
 		case EEQL:
 			if (record->event.pressed) {
@@ -199,51 +151,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			break;
 	}
 	return true;
-}
-
-
-void encoder_update( bool clockwise) {
-	uint16_t held_keycode_timer = timer_read();
-	if (IS_LAYER_ON(_LOWER)) {
-		if (!clockwise) {
-			register_code(KC_VOLU);
-			while (timer_elapsed(held_keycode_timer) < MEDIA_KEY_DELAY) {
-				continue;
-			}
-			unregister_code(KC_VOLU);
-		} else {
-			register_code(KC_VOLD);
-			while (timer_elapsed(held_keycode_timer) < MEDIA_KEY_DELAY) {
-				continue;
-			}
-			unregister_code(KC_VOLD);
-		}
-	} else if (lalt || lctrl) {
-		if (!clockwise) tap_code(KC_TAB);
-		else {
-			register_code(KC_LSFT);
-			tap_code(KC_TAB);
-			unregister_code(KC_LSFT);
-		}
-	} else if (IS_LAYER_ON(_RAISE)) {
-		(!clockwise) ? tap_code(KC_RIGHT) :tap_code(KC_LEFT);
-	} else {
-		if (!clockwise) {
-#ifdef MOUSEKEY_ENABLE
-			register_code(KC_MS_WH_DOWN);
-			unregister_code(KC_MS_WH_DOWN);
-#else
-			register_code(KC_PGDN);
-			unregister_code(KC_PGDN);
-#endif
-		} else {
-#ifdef MOUSEKEY_ENABLE
-			register_code(KC_MS_WH_UP);
-			unregister_code(KC_MS_WH_UP);
-#else
-			register_code(KC_PGUP);
-			unregister_code(KC_PGUP);
-#endif
-		}
-	}
 }

53c0217c79fe02b3dd58021b33715f153542d47c
Amy Dang <kirontoo@gmail.com>
2022-07-26 21:04:39 -0700

    Update firmware and keymap for dz60

--- dz60/config.h
+++ dz60/config.h
@@ -1,14 +1,1 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#include "config.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x2260
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    KBDFans
-#define PRODUCT         DZ60
-#define DESCRIPTION     DZ60 Keyboard
-
-#endif
+#define GRAVE_ESC_GUI_OVERRIDE    # Always send Escape if GUI is pressed
